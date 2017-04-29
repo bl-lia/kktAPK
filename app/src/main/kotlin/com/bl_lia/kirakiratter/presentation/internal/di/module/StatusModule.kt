45cebe6f639d376dbb1b7896542cc6cbdd15796e
bl-lia <mb.bl.lia@gmail.com>
2017-04-29 17:31:20 +0900

    tidyup

--- app/src/main/kotlin/com/bl_lia/kirakiratter/presentation/internal/di/module/StatusModule.kt
+++ app/src/main/kotlin/com/bl_lia/kirakiratter/presentation/internal/di/module/StatusModule.kt
@@ -6,8 +6,16 @@ import com.bl_lia.kirakiratter.domain.executor.ThreadExecutor
 import com.bl_lia.kirakiratter.domain.interactor.SingleUseCase
 import com.bl_lia.kirakiratter.domain.interactor.status.PostStatusUseCase
 import com.bl_lia.kirakiratter.domain.interactor.status.UploadMediaUseCase
+import com.bl_lia.kirakiratter.domain.interactor.timeline.FavouriteStatusUseCase
+import com.bl_lia.kirakiratter.domain.interactor.timeline.ReblogStatusUseCase
+import com.bl_lia.kirakiratter.domain.interactor.timeline.UnfavouriteStatusUseCase
+import com.bl_lia.kirakiratter.domain.interactor.timeline.UnreblogStatusUseCase
+import com.bl_lia.kirakiratter.domain.interactor.translation.TranslateContentUseCase
 import com.bl_lia.kirakiratter.domain.repository.StatusRepository
+import com.bl_lia.kirakiratter.domain.repository.TimelineRepository
+import com.bl_lia.kirakiratter.domain.repository.TranslationRepository
 import com.bl_lia.kirakiratter.domain.value_object.Media
+import com.bl_lia.kirakiratter.domain.value_object.Translation
 import com.bl_lia.kirakiratter.presentation.internal.di.PerFragment
 import dagger.Module
 import dagger.Provides
@@ -36,5 +44,60 @@ class StatusModule {
             postExecutionThread: PostExecutionThread
     ): SingleUseCase<Media> {
         return UploadMediaUseCase(statusRepository, threadExecutor, postExecutionThread)
+    }
+
+    @Provides
+    @PerFragment
+    @Named("favouriteStatus")
+    internal fun provideFavouriteStatus(
+            timelineRepository: TimelineRepository,
+            threadExecutor: ThreadExecutor,
+            postExecutionThread: PostExecutionThread
+    ): SingleUseCase<Status> {
+        return FavouriteStatusUseCase(timelineRepository, threadExecutor, postExecutionThread)
+    }
+
+    @Provides
+    @PerFragment
+    @Named("unfavouriteStatus")
+    internal fun provideUnfavouriteStatus(
+            timelineRepository: TimelineRepository,
+            threadExecutor: ThreadExecutor,
+            postExecutionThread: PostExecutionThread
+    ): SingleUseCase<Status> {
+        return UnfavouriteStatusUseCase(timelineRepository, threadExecutor, postExecutionThread)
+    }
+
+    @Provides
+    @PerFragment
+    @Named("reblogStatus")
+    internal fun provideReblogStatus(
+            timelineRepository: TimelineRepository,
+            threadExecutor: ThreadExecutor,
+            postExecutionThread: PostExecutionThread
+    ): SingleUseCase<Status> {
+        return ReblogStatusUseCase(timelineRepository, threadExecutor, postExecutionThread)
+    }
+
+    @Provides
+    @PerFragment
+    @Named("unreblogStatus")
+    internal fun provideUnreblogStatus(
+            timelineRepository: TimelineRepository,
+            threadExecutor: ThreadExecutor,
+            postExecutionThread: PostExecutionThread
+    ): SingleUseCase<Status> {
+        return UnreblogStatusUseCase(timelineRepository, threadExecutor, postExecutionThread)
+    }
+
+    @Provides
+    @PerFragment
+    @Named("translateContent")
+    internal fun provideTranslateContent(
+            translationRepository: TranslationRepository,
+            threadExecutor: ThreadExecutor,
+            postExecutionThread: PostExecutionThread
+    ): SingleUseCase<List<Translation>> {
+        return TranslateContentUseCase(translationRepository, threadExecutor, postExecutionThread)
     }
 }
